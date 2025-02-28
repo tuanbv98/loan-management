47d04769876057aa9c91363208c65bd468773c63
buivantuan1998 <buivtuan103@gmail.com>
2025-02-28 11:13:07 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+    sameSite: 'strict'
+  })
+);
+
+// database
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+
+app.set("view engine", "ejs");
+
+// Set views directory explicitly (ensure correct path)
+app.set("views", path.join(__dirname, "app/views"));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}

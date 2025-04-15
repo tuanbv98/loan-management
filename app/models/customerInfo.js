2267f033e10b12a4daf4e2f047bef3bb39e43433
buivantuan1998 <buivtuan103@gmail.com>
2025-04-15 23:30:03 +0700

    add table

---
+++ app/models/customerInfo.js
@@ -0,0 +1,72 @@
+module.exports = (sequelize, Sequelize) => {
+  const { DataTypes } = require("sequelize");
+
+  const CustomerInfo = sequelize.define("customer_infos", {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    icloud: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    id_card_front: {
+      type: Sequelize.STRING(255), // đường dẫn ảnh mặt trước
+      allowNull: true
+    },
+    id_card_back: {
+      type: Sequelize.STRING(255), // đường dẫn ảnh mặt sau
+      allowNull: true
+    },
+    id_card_issue_date: {
+      type: Sequelize.DATEONLY, // Ngày cấp
+      allowNull: true
+    },
+    id_card_issue_place: {
+      type: Sequelize.STRING(100), // Nơi cấp
+      allowNull: true
+    },
+    contact_phone_1: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    contact_phone_2: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    contact_phone_3: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    // Thông tin tài khoản ngân hàng
+    bank_name: {
+      type: Sequelize.STRING(100),
+      allowNull: true
+    },
+    bank_account_number: {
+      type: Sequelize.STRING(50),
+      allowNull: true
+    },
+    bank_account_name: {
+      type: Sequelize.STRING(100),
+      allowNull: true
+    },
+    // Thông tin công việc
+    workplace_name: {
+      type: Sequelize.STRING(100), // Tên nơi làm việc
+      allowNull: true
+    },
+    workplace_address: {
+      type: Sequelize.TEXT, // Địa chỉ nơi làm việc
+      allowNull: true
+    },
+  });
+
+  return CustomerInfo;
+};
--- app/models/index.js
+++ app/models/index.js
@@ -26,13 +26,16 @@ db.user = require("./user.js")(sequelize, Sequelize);
 db.customer = require("./customer.js")(sequelize, Sequelize);
 db.loan = require("./loan.js")(sequelize, Sequelize);
 db.payment = require("./payment.js")(sequelize, Sequelize);
+db.customerInfo = require("./customerInfo.js")(sequelize, Sequelize);
 db.transaction = require("./transaction.js")(sequelize, Sequelize);
 
 // Relationships table
 db.customer.hasMany(db.loan, { foreignKey: "user_id" });
 db.customer.hasMany(db.payment, { foreignKey: "user_id" });
+db.customer.hasOne(db.customerInfo, { foreignKey: "user_id" });
 
 db.loan.belongsTo(db.customer, { foreignKey: "user_id" });
-db.payment.belongsTo(db.payment, { foreignKey: "user_id" });
+db.payment.belongsTo(db.customer, { foreignKey: "user_id" });
+db.customerInfo.hasOne(db.customer, { foreignKey: "user_id" });
 
 module.exports = db;
